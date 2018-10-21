1d32e3fe291c32ef5d89be82b7ea633daf7268ec
Lindroy <linyulong2014@163.com>
2018-10-21 16:20:59 +0800

    添加Activity管理器AppManager。

--- baselibrary/src/main/java/com/kotlin/baselibrary/ui/activity/BaseActivity.kt
+++ baselibrary/src/main/java/com/kotlin/baselibrary/ui/activity/BaseActivity.kt
@@ -1,11 +1,23 @@
 package com.kotlin.baselibrary.ui.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.kotlin.baselibrary.common.AppManager
 
 /**
  * @author Lin
  * @date 2018/9/15
- * @function
+ * @function Activity基类
  */
 open class BaseActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppManager.instance.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AppManager.instance.finishActivity(this)
+    }
 }
